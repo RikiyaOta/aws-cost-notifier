b05633b059e4432c000f0ab15c302adc492f9809
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-02-12 17:16:18 +0900

    Implement some functions: Format Message, Post to Slack.

--- src/aws_cost_notifier/core.clj
+++ src/aws_cost_notifier/core.clj
@@ -1,10 +1,16 @@
 (ns aws-cost-notifier.core
-  (:require [clojure.edn :as edn]
+  (:require [clojure.string :as string]
             [amazonica.aws.costexplorer :as costexplorer]
+            [clj-http.client :as client]
             [defun.core :refer [defun]]))
 
+; ---------- Get Data from AWS CostExplorer -------------
+
 ; TODO: Use environment variables.
-(def aws-credentials {:profile "rikiya-dev" :endpoint "ap-northeast-1"})
+
+(defn get-aws-profile [] (System/getenv "AWS_PROFILE"))
+
+(def aws-credentials {:profile (get-aws-profile) :endpoint "ap-northeast-1"})
 
 ; TODO: Get today dynamically.
 (def query-params {:granularity "MONTHLY"
@@ -21,7 +27,7 @@
 #_{:clj-kondo/ignore [:unresolved-symbol]}
 (defun transform-group
   [{:keys [service] :metrics {:NetUnblendedCost {:amount amount-str}}}]
-  {:service service :amount (edn/read-string amount-str)})
+  {:service service :amount amount-str})
 
 (defn- transform-groups [groups] #_{:clj-kondo/ignore [:unresolved-symbol]}
   (map transform-group groups))
@@ -34,3 +40,38 @@
         groups (get-groups results-by-time)]
     {:time-period time-period :groups (transform-groups groups)}))
 
+; --------------- Notify via Slack -------------------
+
+(defn get-max-service-length [groups]
+  (let [services (map #(:service %1) groups)
+        length-list (map count services)]
+    (apply max length-list)))
+
+(defn gen-format-string [max-length] (str "%-" (+ max-length 1) "s"))
+
+(defn gen-group-formatter [max-length]
+  (fn [group] (str (format (gen-format-string max-length) (:service group)) ": " (:amount group) " USD")))
+
+(defn select-groups [groups]
+  (let [f (fn [group] (not= (:amount group) "0"))]
+    (filter f groups)))
+
+#_{:clj-kondo/ignore [:unresolved-symbol]}
+(defun build-notification-message [{:time-period _ :groups groups}]
+  (let [groups (select-groups groups)
+        max-length (get-max-service-length groups)
+        format-group (gen-group-formatter max-length)
+        formatted-groups (map format-group groups)]
+    (str "```" (string/join "\n" formatted-groups) "```")))
+
+(defn get-url [] (System/getenv "INCOMING_WEBHOOK_URL"))
+
+(defn post-to-slack [text]
+  (client/post (get-url) {:content-type :json :form-params {:text text}}))
+
+; ------------------- Main Logic ---------------------
+(defn main []
+  (let [result (get-cost-and-usage)
+        message #_{:clj-kondo/ignore [:unresolved-symbol]}
+        (build-notification-message result)]
+    (post-to-slack message)))
