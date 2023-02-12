d274ec62671372923fe1759847732f04c3495840
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-02-12 16:20:05 +0900

    Implement base functions.

--- project.clj
+++ project.clj
@@ -4,5 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.namespace "1.4.0"]]
-  :repl-options {:init-ns aws-cost-notifier.core})
+                 [org.clojure/tools.namespace "1.4.0"]
+                 [amazonica "0.3.163"]
+                 [defun "0.3.1"]]
+  :repl-options {:init-ns aws-cost-notifier.core}
+  :plugins [[lein-cljfmt "0.9.2"]])
--- src/aws_cost_notifier/core.clj
+++ src/aws_cost_notifier/core.clj
@@ -1,6 +1,36 @@
-(ns aws-cost-notifier.core)
+(ns aws-cost-notifier.core
+  (:require [clojure.edn :as edn]
+            [amazonica.aws.costexplorer :as costexplorer]
+            [defun.core :refer [defun]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; TODO: Use environment variables.
+(def aws-credentials {:profile "rikiya-dev" :endpoint "ap-northeast-1"})
+
+; TODO: Get today dynamically.
+(def query-params {:granularity "MONTHLY"
+                   :metrics ["NetUnblendedCost"]
+                   :time-period {:start "2023-02-01" :end "2023-02-28"}
+                   :group-by [{:key "SERVICE" :type "DIMENSION"}]})
+
+(defn- get-groups [results-by-time]
+  (:groups (first results-by-time)))
+
+(defn- get-time-period [results-by-time]
+  (:time-period (first results-by-time)))
+
+#_{:clj-kondo/ignore [:unresolved-symbol]}
+(defun transform-group
+  [{:keys [service] :metrics {:NetUnblendedCost {:amount amount-str}}}]
+  {:service service :amount (edn/read-string amount-str)})
+
+(defn- transform-groups [groups] #_{:clj-kondo/ignore [:unresolved-symbol]}
+  (map transform-group groups))
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn get-cost-and-usage
+  []
+  (let [{results-by-time :results-by-time} (costexplorer/get-cost-and-usage aws-credentials query-params)
+        time-period (get-time-period results-by-time)
+        groups (get-groups results-by-time)]
+    {:time-period time-period :groups (transform-groups groups)}))
+
